e21594c2b9456c1ac52833fe44a3d74bb7b431f6
Suraj Gaire <eriag321@gmail.com>
2022-09-25 16:14:06 +0545

    changes in json file

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,31 +1,47 @@
 import React from "react";
 import menuItems from "../utils/menuItems.json";
 import "./style.css";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 
 function Menu() {
-//   let items = [];
-    for (let key in menuItems) {
-      console.log(key);
-      console.log(menuItems[key]);
-    }
-    // console.log(menuItems)
+  let keys = [];
 
-//   for (let key in menuItems) {
-//     items.push([key, menuItems[key]]);
-//   }
-  // console.log(items[0][0])
+  // console.log(JSON.parse(menuItems));
+
+  for (let [key,value] of Object.entries(menuItems)) {
+    console.log(key);
+    console.log(value);
+  }
 
   return (
-    <div className="menu__container">
-      <div className="menu">
-        <h1>Our Menu</h1>
-        {/* <h3>Momo</h3>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <p>Steam</p>
-          <p>130</p>
-        </div> */}
-      </div>
-    </div>
+    // <div className="menu__container">
+    //   <div className="menu">
+    //     <h1>Our Menu</h1>
+    //     {/* <h3>Momo</h3>
+    //     <div style={{ display: "flex", justifyContent: "space-between" }}>
+    //       <p>Steam</p>
+    //       <p>130</p>
+    //     </div> */}
+    //     {menuItems.map((menu, i) => {
+    //       // console.log(menu);
+    //       return (
+    //         <div key={i}>
+    //           <h3>{menu.category}</h3>
+    //           <div style={{ display: "flex", justifyContent: "space-between" }}>
+    //             <p>{}</p>
+    //             <p>{}</p>
+    //           </div>
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ width: "80%", margin: "3em auto" }}>
+        <Typography component={"h1"} variant = "p">Our Menu</Typography>
+      </Box>
+    </Box>
   );
 }
 
